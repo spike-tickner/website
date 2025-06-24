53af2b1f48951a9a2030bba1ca8c65473762ec8b
Spike <spike.tickner@gmail.com>
2025-06-24 15:20:56 +1000

    Initial

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
+};
